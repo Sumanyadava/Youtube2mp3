23f82006c4aea1d72194eb821f970aedfd3fd6ac
Sumanyadava <ansum2411@gmail.com>
2024-01-19 22:54:07 +0530

    build: youtube2mp3 and added rapid api

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import axios from "axios";
+import { useRef, useState } from "react";
+import { youtube_parser } from "./utils";
+
+function App() {
+  const inptUrlRef = useRef();
+
+  const [urlResult, setUrlResult] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const youtubeID = youtube_parser(inptUrlRef.current.value);
+    console.log({ youtubeID });
+
+    const options = {
+      method: "GET",
+      url: "https://youtube-mp36.p.rapidapi.com/dl",
+
+      headers: {
+        "X-RapidAPI-Key": "7ebea07ef4msh1bb9847eba8efcfp109790jsnb75bcd5e429c",
+        "X-RapidAPI-Host": "youtube-mp36.p.rapidapi.com",
+      },
+      params: { id: youtubeID },
+    };
+
+    axios(options)
+      .then((res) => {
+        console.log(res);
+
+        setUrlResult(res.data.link);
+      })
+      .catch((err) => console.log(err));
+
+    inptUrlRef.current.value = "";
+  };
+  return (
+    <>
+      <span>youtube2mp3</span>
+      <section>
+        <h1>youtube2mp3converter</h1>
+        <p className="desc">trakajha kdikjashdkajs</p>
+
+        <form action="" onSubmit={handleSubmit}>
+          <input ref={inptUrlRef} type="text" placeholder="place of hold" />
+          <button type="submit"></button>
+        </form>
+        {urlResult ? (
+          <a target="_blank" rel="noreffer" href={urlResult}>
+            Download MP3
+          </a>
+        ) : (
+          ""
+        )}
+      </section>
+    </>
+  );
+}
+
+export default App;
